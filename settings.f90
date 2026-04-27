b68a360ef408111024af0e42f45e7cb43e442324
Mirko-okriM <152259506+Mirko-okriM@users.noreply.github.com>
2026-04-27 10:03:57 +0200

    Update settings.f90

--- settings.f90
+++ settings.f90
@@ -1,28 +1,29 @@
-!-----------------------------------------------------------------------------------------------------------------------------------------!
-!--------- USER INPUT FILE ---------------------------------------------------------------------------------------------------------------! 
-!-----------------------------------------------------------------------------------------------------------------------------------------!
-! - compile solver with: gfortran.exe -fopenmp -O3 permittivitySolver.f90 -o solverRun.exe
-! - number of cpus/threads has to be adjusted with environment variable OMP_NUM_THREADS, e.g. on windows 11 --> $env:OMP_NUM_THREADS=8     
+!----------------------------------------------------------------------------------------------------------------!
+!--------- USER INPUT FILE --------------------------------------------------------------------------------------! 
+!----------------------------------------------------------------------------------------------------------------!
 
-integer, parameter :: nx = 50                                                                                                  !Image width
-integer, parameter :: ny = 50                                                                                                  !Image height
-integer, parameter :: nz = 50                                                                                                  !Image depth
-integer, parameter :: voxelRes = 1 !DO NOT CHANGE                                                                                 !VoxelResolution of input-raw data --> SHOULD STAY 1, VALUE DOES NOT REALLY HAVE INFLUENCE ON RESULT, MIGHT BE CHANGED TO TYPE REAL IN FUTURE VERSION
-integer, parameter :: realPrecision = 2                                                                                          !floating-point precision: 1 (single) or 2 (double)
-integer, parameter :: nIteration = 100000                                                                                         !max. number of iterations
-real(kind=4*realPrecision), parameter :: targetResidual = 1e-30                                                                    !target residual    
-integer(kind=1), parameter :: bitLength = 8                                                                                     !Bites per pixel, typically grey-values in raw images are stored as either 8-bit or 16-bit integers
-integer(kind=1), parameter :: nPhases = 2 
-integer, dimension(2) :: rawValues = [0,255]                                                                                      !bit-grayvalues of each phase (unsigned format)
-real(kind=4*realPrecision), dimension(nPhases) :: correspConductivity = [1.0,1e-6]                                                       !conductivity of each phase (only double values, e.g. 1.0)
-real(kind=4*realPrecision), dimension(nPhases) :: correspPermittivity = [79.0,4.5]                                                       !conductivity of each phase (only double values, e.g. 1.0)
-real(kind=4*realPrecision), parameter :: frequency = 1.01e8                                                                     !frequency   
-character(512) :: casePath = 'C:\Users\Mirko\Desktop\solver\solver_permittivity\samples\'       !END STRING WITH SLASH             !path to case folder (place raw-data in this folder, results will as be saved there as well)
-character(512) :: sampleName = 'veldsteen_poreus_50cube.raw'                                                                   !input file (veldsteen_poreus_250cube, Berea_2d25um_binary_400cube, spheres_150cube)
-character(1) :: evalDirection = 'Z'                                                                                             !direction of calculation (X or Y or Z)
-logical :: writeTempertureField = .FALSE.                                                                                         !write calculated temperature-field (.TRUE. or .FALSE.)
-logical :: writeFluxField = .FALSE.                                                                                               !write calculated flux-field (.TRUE. or .FALSE.)
+integer, parameter :: nx = 400                                             		! Image width
+integer, parameter :: ny = 400                                             		! Image height
+integer, parameter :: nz = 400                                             		! Image depth
+integer, parameter :: voxelRes = 1 !DO NOT CHANGE                           	! VoxelResolution of input-raw data --> SHOULD STAY 1, VALUE DOES NOT REALLY HAVE INFLUENCE ON RESULT, MIGHT BE CHANGED TO TYPE REAL IN FUTURE VERSION
+integer, parameter :: realPrecision = 2                                    		! Floating-point precision: 1 (single) or 2 (double)
+integer, parameter :: nIteration = 500000                                      	! Max. number of iterations
+real(kind=4*realPrecision), parameter :: targetResidual = 1e-9            		! Target residual    
+integer(kind=1), parameter :: bitLength = 8                                 	! Bites per pixel, typically grey-values in raw images are stored as either 8-bit or 16-bit integers
+integer(kind=1), parameter :: nPhases = 5 										! Number of phases in model
+integer, dimension(nPhases) :: rawValues = [1, 2, 3, 4, 5]                      ! Bit-grayvalues of each phase (unsigned format)
+real(kind=4*realPrecision), dimension(nPhases) :: correspConductivity = [3.98E-15, 3.89E-05, 3.85E-05, 1.25E-05, 5.70E-12]  ! Conductivity of each phase (only double values, e.g. 1.0)
+real(kind=4*realPrecision), dimension(nPhases) :: correspPermittivity = [1.00, 9.65, 3.42, 2.74, 3.49]  					! Conductivity of each phase (only double values, e.g. 1.0)
+real(kind=4*realPrecision), parameter :: frequency = 1e9                  		! Frequency   
+character(512) :: casePath = "/Path/to/case/folder/" ! END STRING WITH SLASH    ! Path to case folder (place raw-data in this folder)
+character(512) :: sampleName = "RotondoGranite_Cube400_8bit.raw"                ! Input file
+character(1) :: evalDirection = "X"                                         	! Direction of calculation (X or Y or Z)
+character(len=20) :: precondType="gamg"											! Choose preconditioner (see https://petsc.org/release/manualpages/PC/PCType/)
+character(len=20) :: solverType="bcgs"											! Choose solver (see https://petsc.org/main/manualpages/KSP/KSPType/)
 
-!----------------------------------------------------------------------------------------------------------------------------------------!
-!--------- Feel free to contact me. I will be happy to help: mirko.siegert@web.de -------------------------------!
-!----------------------------------------------------------------------------------------------------------------------------------------!
+!----------------------------------------------------------------------------------------------!
+!--------- Feel free to contact us. We will be happy to help: 	-------------------------------!
+!--------- mirko.siegert@web.de 								-------------------------------!
+!--------- nils.kerkmann@ieg.fraunhofer.de		 				-------------------------------!
+!----------------------------------------------------------------------------------------------!
+        
